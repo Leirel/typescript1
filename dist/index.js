a77ed289be5958bc5984a0d5c2706ba135c239fb
HikiLei <y2508189@gmail.com>
2025-10-01 11:16:38 +0900

    깃허브 연결

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+//console.log("Hello, TypeScript");
+Object.defineProperty(exports, "__esModule", { value: true });
+//const nums: number[] = [];
+//const a = 10 + nums[0]
+//const somestring = "typescript";
+//somestring.endsWith("!")
+const util_js_1 = require("./util.js");
+(0, util_js_1.printName)("typescript");
